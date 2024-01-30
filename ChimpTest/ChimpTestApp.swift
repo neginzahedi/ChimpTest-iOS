852efd3b71be987a128f92fd9b3ea47b56ea7cd7
Negin Zahedi <zahedi.negin@gmail.com>
2024-01-30 17:06:55 -0500

    Initial Commit

---
+++ ChimpTest/ChimpTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChimpTestApp.swift
+//  ChimpTest
+//
+//  Created by Negin Zahedi on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct ChimpTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
